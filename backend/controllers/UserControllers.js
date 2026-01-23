066e4261e82276c074019e63837c6414c1decd92
Nagender211 <dnagender2019@gmail.com>
2026-01-23 09:17:56 +0530

    user resgistre and login

--- backend/controllers/UserControllers.js
+++ backend/controllers/UserControllers.js
@@ -1,9 +1,75 @@
+import User from '../models/UserModel.js'
 export const Testing=(req,res)=>{
     try {
         res.send({message: "hello world"})
     } catch (error) {
         console.log("error while testing",error)
         
+    }
+}
+
+export const Register = async(req,res)=>{
+    try {
+        const {username,email,password,confirmpassword}=req.body;
+        if(!username || !email || !password || !confirmpassword){
+            return res.status(404).json({
+                message: "please enter the all the fields"
+            })
+        }
+        if(password!==confirmpassword){
+            return res.status(404).json({
+                message: "please match the password"
+            })
+        }
+        const exitinguser=await User.findOne({email})
+        if(exitinguser){
+            return res.status(409).json({
+                message: "user is already exists"
+            })
+        }
+        const user=await User.create({username,email,password,confirmpassword})
+        res.status(201).json({
+            message: "succesfully created user",
+            data: user
+        })
+    } catch (error) {
+        console.log("error while creating user",error)
+        return res.status(501).json({
+                message: "Something went worng please try again"
+        })
+        
+    }
+}
+
+export const Login =async (req,res)=>{
+    
+    try {
+        const {email,password}=req.body;
+        if(!email || !password){
+            return res.status(404).json({
+                message: "please enter the all the fields"
+            })
+        }
+        const user=await User.findOne({email});
+        if(!user){
+          return res.status(404).json({
+                message: "user is not found please register"
+            })
+        }
+        if(user.password!==password){
+            return res.status(404).json({
+                message: "password is incrroct"
+            })
+        }
+        res.status(200).json({
+            message: "login is succefully",
+            data: user
+        })
+    } catch (error) {
+        console.log("error while login",error)
+         return res.status(501).json({
+                message: "Something went worng please try again"
+        })
     }
 }
 
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,6 +1,11 @@
 import express from 'express'
-import { Testing } from '../controllers/UserControllers.js'
+import { Login, Register, Testing } from '../controllers/UserControllers.js'
 const router=express.Router()
 router.get('/',Testing)
+
+// user routeing
+router.post('/register',Register)
+router.post('/login',Login)
+
 
 export default router;
